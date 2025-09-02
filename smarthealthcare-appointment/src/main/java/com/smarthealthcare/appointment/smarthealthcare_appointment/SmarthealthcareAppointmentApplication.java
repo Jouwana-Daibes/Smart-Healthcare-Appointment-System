2255ab129b530e628cc488ef7db7ff82dd06286d
Jouwana Daibes <jouwanadaibes@gmail.com>
2025-09-02 16:42:04 +0300

    initial commit

---
+++ smarthealthcare-appointment/src/main/java/com/smarthealthcare/appointment/smarthealthcare_appointment/SmarthealthcareAppointmentApplication.java
@@ -0,0 +1,13 @@
+package com.smarthealthcare.appointment.smarthealthcare_appointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarthealthcareAppointmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarthealthcareAppointmentApplication.class, args);
+	}
+
+}
---
+++ smarthealthcare-appointment/src/test/java/com/smarthealthcare/appointment/smarthealthcare_appointment/SmarthealthcareAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarthealthcare.appointment.smarthealthcare_appointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarthealthcareAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
